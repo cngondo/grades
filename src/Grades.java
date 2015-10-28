f562c6f2b5b07c2ca8a529d2dbc317403ce5ffb9
cngondo <devngondo@gmail.com>
2015-10-28 20:01:12 +0300

    simple grading program in java

---
+++ src/Grades.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+/**
+ * Created by ngondo on 10/28/15.
+ */
+
+//Takes user input and gives grade of student depending on the marks
+public class Grades {
+    public static void main(String[]args){
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter the grade of the student");
+        int grade = scanner.nextInt();
+
+        if(grade >=80 && grade <= 100)
+        {System.out.println("A");}
+        else if(grade >=65 && grade<= 79)
+        {System.out.println("B");}
+        else if(grade >=55 && grade<= 64)
+        {System.out.println("C");}
+        else if(grade >=40 && grade<= 54)
+        {System.out.println("D");}
+        else if(grade >=40 && grade<= 0)
+        {System.out.println("Fail");}
+        else
+        { System.out.println("Invalid Marks");}
+    }
+}
